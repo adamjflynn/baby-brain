d76330db87b5db841f5e1b01346affbe696dbca4
aziz <ahmed613@umn.edu>
2021-11-27 20:00:45 -0600

    event get-routes

--- routes/api/eventRoutes.js
+++ routes/api/eventRoutes.js
@@ -1,3 +1,9 @@
+const router = require('express').Router();
+const { Event, Baby } = require('../../models');
+
+
+
+
 router.get('/', (req, res) => {
     Event.findAll({
       include: [
@@ -13,4 +19,41 @@ router.get('/', (req, res) => {
       console.log(err)
       res.status(500).json(err)
     })
-  });
+});
+
+router.get('/:id', (req, res) => {
+  Event.findOne({
+    include: [
+      {
+        model: Baby,
+        attributes: ['baby_name']
+      }
+    ],
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbEventData => {
+      if (!dbEventData) {
+        res.status(404).json({ message: 'No Matches' });
+        return;
+      }
+      res.json(dbEventData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.post('/', (req, res) => {
+  Event.create({
+      note: req.body.note
+  })
+      .then(dbEventData => res.json(dbEventData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
