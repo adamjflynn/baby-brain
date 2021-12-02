ec566fdc0ad8089ce7da9a7bfc0907a75d54e90c
Adam Flynn <adamflynn1@gmail.com>
2021-12-01 18:16:04 -0600

    uncomment login function

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -1,50 +1,50 @@
 async function loginFormHandler(event) {
-  //   event.preventDefault();
+    event.preventDefault();
   
-  //   const username = document.querySelector('.username').value.trim();
-  //   const password = document.querySelector('.password').value.trim();
+    const username = document.querySelector('.username').value.trim();
+    const password = document.querySelector('.password').value.trim();
   
-  //   if (username && password) {
-  //     const response = await fetch('/api/users/login', {
-  //       method: 'post',
-  //       body: JSON.stringify({
-  //         username,
-  //         password
-  //       }),
-  //       headers: { 'Content-Type': 'application/json' }
-  //     });
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
   
-  //     if (response.ok) {
-  //       document.location.replace('./html/user.html');
-  //     } else {
-  //       alert(response.statusText);
-  //     }
-  //   }
+      if (response.ok) {
+        document.location.replace('./html/user.html');
+      } else {
+        alert(response.statusText);
+      }
+    }
   console.log("test");
    }
   
   async function signupFormHandler(event) {
-   // event.preventDefault();
+   event.preventDefault();
   
-    // const username = document.querySelector('.username').value.trim();
-    // const password = document.querySelector('.password').value.trim();
+    const username = document.querySelector('.username').value.trim();
+    const password = document.querySelector('.password').value.trim();
   
-    // if (username && password) {
-    //   const response = await fetch('/api/user', {
-    //     method: 'post',
-    //     body: JSON.stringify({
-    //       username,
-    //       password
-    //     }),
-    //     headers: { 'Content-Type': 'application/json' }
-    //   });
+    if (username && password) {
+      const response = await fetch('/api/user', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
   
-    //   if (response.ok) {
-    //     document.location.replace('./html/user.html');
-    //   } else {
-    //     alert(response.statusText);
-    //   }
-    // }
+      if (response.ok) {
+        document.location.replace('./html/user.html');
+      } else {
+        alert(response.statusText);
+      }
+    }
     console.log("test");
   }
 
