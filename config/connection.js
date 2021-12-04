7487d9cee2cfe82f6476638bae0c5fce76d1ea46
Adam Flynn <adamflynn1@gmail.com>
2021-12-03 18:47:58 -0600

    trial handlebars

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,10 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306
+    port: 3306,
+    user: DB_USER,
+    database: DB_NAME,
+    password: DB_PW
 });
 
 
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const PORT = 3001;
 const app = express();
+const exphbs = require('express-handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+app.engine('hbs', exphbs ({
+    
+}))
 
-
-
+app.get('/', function (req, res) {
+    res.render('index')
+})
 
 app.listen(PORT, () => {
-    console.log('app is listening on port')
+    console.log(`app is listening on port http://localhost:${PORT}`)
 })
