d6ce1cfdbc775ab82d0db815ff330e0a7b1692b7
Tony <abusch@css.edu>
2021-12-05 23:55:48 -0600

    change to trick heroku

--- public/assets/javascript/filterEvents.js
+++ public/assets/javascript/filterEvents.js
@@ -1,39 +1,39 @@
-async function filterEventsFunction(event) {
-    event.preventDefault();
+// async function filterEventsFunction(event) {
+//     event.preventDefault();
   
-    console.log('testfilter1')
+//     console.log('testfilter1')
   
-    const event_type = document.getElementById('event-type-filter').value
-    const baby_id = document.getElementById('child-filter').value
-    console.log(event_type)
-    console.log(baby_id)
+//     const event_type = document.getElementById('event-type-filter').value
+//     const baby_id = document.getElementById('child-filter').value
+//     console.log(event_type)
+//     console.log(baby_id)
   
    
-    if(event_type && baby_id) {
-      const response = await fetch(`/api/events/`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          event_type,
-          baby_id
+//     if(event_type && baby_id) {
+//       const response = await fetch(`/api/events/`, {
+//         method: 'PUT',
+//         body: JSON.stringify({
+//           event_type,
+//           baby_id
           
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
+//         }),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         }
+//       })
      
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
+//       if (response.ok) {
+//         document.location.replace('/');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
    
-    console.log('testfilter2')
-  }
+//     console.log('testfilter2')
+//   }
 
 
 
 
 
-document.querySelector('#filter-form').addEventListener('submit', filterEventsFunction)
+// document.querySelector('#filter-form').addEventListener('submit', filterEventsFunction)
