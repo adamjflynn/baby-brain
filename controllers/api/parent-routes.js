a5709e87d02953220dc1f7b0dcb0b8e9306383cb
Tony <abusch@css.edu>
2021-12-05 22:21:11 -0600

    heroku fix

--- controllers/api/parent-routes.js
+++ controllers/api/parent-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Parent, Baby } = require('../../models/index.js');
+const { Parent, Baby } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
