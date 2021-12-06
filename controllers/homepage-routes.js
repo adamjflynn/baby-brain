0b77da4831712a0273e22d2f61a668899186bab3
Tony <abusch@css.edu>
2021-12-06 00:11:40 -0600

    fix capitalization

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 const withAuth = require('../utils/auth');
-const {Baby,Event,Parent} = require('../Models')
+const {Baby,Event,Parent} = require('../models')
 
 // router.get('/',withAuth, (req, res) => {
 //     res.sendFile(path.join(__dirname, '../public/assets/html/user.html')) 
